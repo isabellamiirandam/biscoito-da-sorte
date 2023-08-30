c7031374ab96f78f3c96e2f81e816db75afddd84
Isabella Miranda <isabellamiirandam@gmail.com>
2023-08-30 12:11:58 -0300

    criando função reloadingPage

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ let luck = document.querySelector('.luck')
 
 // Funções
 function handleClick() {
-
-
   home.classList.add('hide')
   luck.classList.remove('hide')
 }
 
+function reloadingPage() {
+  home.classList.remove('hide')
+  luck.classList.add('hide')
+}
