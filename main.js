7d5a04929e92390230dad715131f6a568e01d1fe
Isabella Miranda <isabellamiirandam@gmail.com>
2023-09-08 11:45:03 -0300

    refatorando, criando número randômico e switch

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-// Variáveis
-let home = document.querySelector('.home')
-let luck = document.querySelector('.luck')
+// Elements
+const home = document.querySelector('.home')
+const luck = document.querySelector('.luck')
+const randomPhraseElement = document.querySelector('#random-phrase')
 
-
-// Funções
-function handleClick() {
+// Functions
+function goToPhrasePage() {
   home.classList.add('hide')
   luck.classList.remove('hide')
 }
 
-function reloadingPage() {
+function goToHomePage() {
   home.classList.remove('hide')
   luck.classList.add('hide')
 }
+
+function generateRandomPhrase() {
+  const randomNumber = Math.round(Math.random() * 4);
+
+  switch (randomNumber) {
+    case 1:
+      randomPhraseElement.innerText = `"As pessoas costumam dizer que a motivação não dura sempre.
+       Bem, nem o efeito do banho, por isso recomenda-se diariamente"`;
+      break;
+    case 2:
+      randomPhraseElement.innerText = `"Nossos fracassos, às vezes, são mais frutíferos do que os êxitos"`;
+      break;
+    case 3:
+      randomPhraseElement.innerText = `"É costume de um tolo, quando erra, queixar-se dos outros.
+       É costume de um sábio queixar-se de si mesmo"`;
+      break;
+    case 4:
+      randomPhraseElement.innerText = `"O insucesso é apenas uma oportunidade para recomeçar com mais inteligência." `;
+      break;
+    default:
+      randomPhraseElement.innerText = `"É sempre divertido fazer o impossível."`;
+  }
+}
+// Handlers
+function onGeneratePhraseButtonClick() {
+  goToPhrasePage()
+  generateRandomPhrase()
+}
+
+function onReopenCookieButtonClick() {
+  goToHomePage()
+}
+
